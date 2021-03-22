dd569062e342bf15e8d1b45294e2d5c6de8f0ad2
Alejandra García <agarcia_pacheco@hotmail.com>
2021-03-22 17:44:53 -0600

    Se identifican los archivos .md, se ingresa a su interior y se lee su contenido

--- index.js
+++ index.js
@@ -2,59 +2,19 @@ const chalk = require('chalk');
 const fs = require('fs');
 const path = require('path');
 
-//Obtener el contenido de cada archivo
-
+//Obtener el contenido de los archivos .md
 const readContentMD = (archive) => {
 	let readArchive = fs.readFileSync(archive, 'utf-8')
 	console.log(chalk.magenta(readArchive))
 }
 
-
-//Entrar a las carpetas y archivos
-
-// const enterFile = (archive) => {
-// 	fs.readdir(archive, (err, files) => {
-// 		if (err){
-// 			return console.log(chalk.red('Error al imprimir los archivos'));			
-// 		}	else {	
-// 			files.forEach((doc) => {
-// 				const docNamePath = path.extname(doc)				
-// 				if(docNamePath == '.md'){
-// 				const newPath = archive + '/' + doc	
-// 				read(newPath);	
-// 			} else if (docNamePath == ''){
-// 				console.log(chalk.yellow('Soy una carpeta', doc))
-// 				const newPath = archive + '/' + doc
-// 				// console.log(chalk.cyan('Estamos accediendo a la ruta:', newPath)) --> Nos muestra la ruta a  que accedemos
-// 				enterFile(newPath)
-// 			}
-// 			})
-// 			// console.log(files) --> Nos muestra todas las carpetas y archivos (sin importar la extensión) de la ruta
-// 		}		
-// 	})
-// }
-
-// const contentArchive = (archive) => {
-// 	fs.readdir(archive, (err, files) => {
-// 		if(err){
-// 			console.log(chalk.red('Error al obtener los archivos de la carpeta'))
-// 		}else{
-// 			files.forEach((doc) => {
-// 				const newPath = archive + '/' + doc
-// 				read(newPath)
-// 				// console.log(newPath)
-// 				// read(newPath)
-// 			})			
-// 		}		
-// 	})
-// }
-
+//Ingresar a los archivos y obtener su contenido
 const contentArchive = (archive) => {
 	fs.readdir(archive, (err, files) => {
 		if(err){
 			console.log(chalk.red('Error al imprimir los archivos'))
 		}else{
-			console.log(chalk.magenta(files))
+			// console.log(chalk.green(files))
 			files.forEach((doc) => {
 				const newPath = archive + '/' + doc
 				extensionArchive(newPath)
@@ -64,17 +24,17 @@ const contentArchive = (archive) => {
 	
 }
 
-
+//Obteniendo la extensión (archivos o carpetas)
 const extensionArchive = (archive) => {
 	const extNamePath = path.extname(archive);
+
 	if (extNamePath == '.md') {
-		console.log(chalk.yellow('Soy un archivo .md'));
+		console.log(chalk.yellow('Soy un archivo con extensión .md'));
 		readContentMD(archive)
-		// readDocMd(archive);
+
 	} else if (extNamePath == '') {
 		console.log(chalk.cyan('Soy una carpeta'));
-		contentArchive(archive)
-		// readDirectory(archive);
+		contentArchive(archive)		
 	}
 }
 
